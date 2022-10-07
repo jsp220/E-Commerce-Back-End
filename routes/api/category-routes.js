66bf1dbf3e5b33efe6e2049ba3d90ce4e9848c46
Joon Park <jsp220@gmail.com>
2022-10-06 23:06:22 -0700

    Write post, put, and delete routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,16 +33,40 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json({message: "Category successfully added"});
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json({message: "Category successfully deleted"});
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 module.exports = router;
