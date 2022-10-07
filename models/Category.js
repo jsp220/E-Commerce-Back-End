9cb22c94af6c6df74234d6c0ce9182459e25273e
Joon Park <jsp220@gmail.com>
2022-10-06 21:35:59 -0700

    Build models for the products, categories, and tags

--- models/Category.js
+++ models/Category.js
@@ -6,14 +6,23 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    categoryName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: 'category'
   }
 );
 
